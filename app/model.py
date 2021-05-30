109fe577e2f583bd889e35d36832051cc0647fa0
Raymond-Hsu0902 <52566240+Raymond-Hsu0902@users.noreply.github.com>
2021-05-30 19:49:28 +0800

    Update model.py

--- app/model.py
+++ app/model.py
@@ -6,7 +6,7 @@ import joblib
 # 載入Model
 #with gzip.open('app/model/xgboost-iris.pgz', 'r') as f:
     #xgboostModel = pickle.load(f)
-xgboostModel = joblib.load("../xgb1.joblib.dat")
+xgboostModel = joblib.load("app/model/xgb1.joblib.dat")
 
 def predict(input):
     pred=xgboostModel.predict(input)[0]
